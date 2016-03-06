cc5664a0bfd668c6c3845d3909f568f8b7b2c9ae
pskeba1 <pskeba1@jhu.edu>
2016-03-06 18:02:09 -0500

    finished first try

--- matplm_main.m
+++ matplm_main.m
@@ -1,4 +1,4 @@
-function [plm_outputs] = matplm_main(psg_struct,varargin)
+function [plm_outputs,lEMG,rEMG] = matplm_main(psg_struct,varargin)
 %% [plm_outputs] = matplm_main(psg_struct)
 % The main driving function for the MATPLM program. Input is the full
 % subject structure, transferred from EDF format with EDF
@@ -7,12 +7,13 @@ function [plm_outputs] = matplm_main(psg_struct,varargin)
 % TODO: add support for one leg? Was thinking I would just make a separate
 % file for that
 sep_flag = 0;
+plm_outputs = struct();
 
 addpath('helper_functions')
 
-% TODO: Eventually, we'll run this file differently for seperate legs
+% TODO: Eventually, we'll run this file differently for separate legs
 if strcmp('separate_legs',varargin)
-    display('MATPLM will calculate CLM/PLM for each leg seperately');
+    display('MATPLM will calculate CLM/PLM for each leg separately');
     sep_flag = 1;
 end
 
@@ -23,6 +24,7 @@ else
     params = getInput(1);
 end
 
+%% Extract necessary data from structure
 % find the LAT and RAT channels in the structure
 % TODO: support different channel naming styles
 lbls = extractfield(psg_struct.Signals,'label');
@@ -38,6 +40,7 @@ rEMG = psg_struct.Signals(ridx(1)).data;
 [epochStage,rec_start,rec_end,apnea_data,arousal_data,start_time] = ...
     errcheck_ss(psg_struct,lEMG,rEMG,params.fs);
 
+%% Filter and rectify data. Optionally apply dynamic threshold
 % Truncate, filter and rectify EMG signals.
 lEMG = butter_rect(params,lEMG,rec_start,rec_end);
 rEMG = butter_rect(params,rEMG,rec_start,rec_end);
@@ -56,6 +59,7 @@ else
     rminT = scanning2(rEMG,params.fs);
 end
 
+%% Calculate leg movements for each leg channel
 % Find all leg movements on the left and right legs.
 min_below = 0.5; % time below low threshold to end movement
 min_above = 0.5; % time above low threshold to start movement
@@ -66,19 +70,57 @@ rLM = findIndices(nrEMG,rminT,rminT+6,min_below,min_above,params.fs);
 
 % optionally apply morphology criterion
 if params.morph == 1
+    plm_outputs.morphology_criterion = 'True';
     lLM = cutLowMedian(lEMG,lLM,lminT,params.fs);
-    rLM = cutLowMedian(rdata,rLM,rminT,params.fs);    
+    rLM = cutLowMedian(rEMG,rLM,rminT,params.fs);    
 end
 
+% we always want these in the output array
+plm_outputs.lLM = lLM;
+plm_outputs.rLM = rLM;
+
+%% Calculate candidate LMs and PLMs for combined/separate legs
 % Here is where things diverge if the user specifies 'separate_legs'
-if sep_legs == 0
+% TODO: what do I do about marking the PLM column in the CLM matrix?
+if sep_flag == 0
     % calculate PLM candidates by combining the legs
-    CLM = combined_candidates(rLM,lLM,apnea_data,arousal_data,start_time,params);
+    CLM = combined_candidates(rLM,lLM,epochStage,apnea_data,arousal_data,start_time,params);
+    [PLM,~] = periodic_lms(CLM,params);
+    [~,ia,~] = intersect(CLM(:,1),PLM(:,1));
+    CLM(ia,5) = 1; % go back and mark PLM in CLM
+    
+    % store output vectors in struct
+    plm_outputs.CLM = CLM;
+    plm_outputs.PLM = PLM;
+    plm_outputs.PLMS = PLM(PLM(:,6) > 0,:);
+    plm_outputs.CLMS = CLM(CLM(:,6) > 0,:);
 else
     % get CLM from each leg seperately
-    lCLM = separate_candidates(lLM,apnea_data,arousal_data,start_time,params);
-    rCLM = separate_candidates(rLM,apnea_data,arousal_data,start_time,params);
+    lCLM = separate_candidates(lLM,epochStage,apnea_data,arousal_data,start_time,params);
+    rCLM = separate_candidates(rLM,epochStage,apnea_data,arousal_data,start_time,params);
+    
+    [lPLM,~] = periodic_lms(lCLM,params);
+    [~,ia,~] = intersect(lCLM(:,1),lPLM(:,1));
+    lCLM(ia,5) = 1; 
+    [rPLM,~] = periodic_lms(rCLM,params);
+    [~,ia,~] = intersect(rCLM(:,1),rPLM(:,1));    
+    rCLM(ia,5) = 1;
+    
+    % store output vectors in struct
+    plm_outputs.lCLM = lCLM;
+    plm_outputs.lPLM = lPLM;
+    plm_outputs.lPLMS = lPLM(lPLM(:,6) > 0,:);
+    plm_outputs.lCLMS = lCLM(lCLM(:,6) > 0,:);
+    
+    plm_outputs.rCLM = rCLM;
+    plm_outputs.rPLM = rPLM;
+    plm_outputs.rPLMS = rPLM(rPLM(:,6) > 0,:);
+    plm_outputs.rCLMS = rCLM(rCLM(:,6) > 0,:);
 end
+
+plm_outputs.column_headers = {'Start','End','Duration','IMI','isPLM','SleepStage',...
+    'Start_in_Min','StartEpoch','Breakpoint','Area','isApnea','isArousal',...
+    'Lateraltiynessment'};
 
 end
 
