896a93f741b4b1b5338228b2eaf44a120f8e5f2f
pskeba1 <pskeba1@jhu.edu>
2016-05-17 12:55:37 -0400

    improved combination code

--- helper_functions/candidate_lms_rev1.m
+++ helper_functions/candidate_lms_rev1.m
@@ -8,7 +8,19 @@ elseif isempty(lLM)
     CLM = rLM(rLM(:,3) < params.maxdur,:);
 else
     % Combine left and right and sort.
-    CLM = rOV(lLM,rLM,params.fs);
+    CLM = rOV2(lLM,rLM,params.fs);
+    
+    CLM(:,3) = (CLM(:,2) - CLM(:,1))/params.fs;
+    
+    % add breakpoints if the duration of the combined movement is greater
+    % than 15 seconds (standard) or if a bilateral movement is made up of
+    % greater than 4 (standard) monolateral movements. These breakpoints
+    % are actually added to the subsequent movement, and the un-CLM is
+    % removed.
+    CLM(find(CLM(1:end-1,4) > params.maxcomb |...
+        CLM(1:end-1,3) > params.maxdur)+1,9) = 1;
+    CLM(CLM(1:end,4) > params.maxcomb |...
+        CLM(1:end,3) > params.maxdur,:) = [];
 end
 
 % If there are no CLM, return an empty vector
@@ -36,62 +48,31 @@ end
 
 end
 
-
-
-function [CLM] = rOV(lLM,rLM,fs)
+function [CLM] = rOV2(lLM,rLM,fs)
 %% REMOVEOVERLAP combine left and right leg LMs
 
-CLM = [];
-max_num = 4; % the maximum allowable unilateral mvmts to make a bilateral
-
 % combine and sort LM arrays
-rLM(:,5) = 1; lLM(:,5) = 2;
+rLM(:,13) = 1; lLM(:,13) = 2;
 combLM = [rLM;lLM];
 combLM = sortrows(combLM,1);
 
 % distance to next movement
-combLM(1:end-1,3) = combLM(2:end,1) - combLM(1:end-1,2);
-combLM(1:end-1,4) =  combLM(1:end-1,3) < fs/2;
+CLM = combLM;
+CLM(:,4) = 1;
 
-% iterate through the combined rLM and lLM array
 i = 1;
-while i <= size(combLM,1)
-    % If the last movement was combined, skip this one (it's already part
-    % of it!)
-    if i > 1 
-        if combLM(i-1,4) == 1
-            i = i+1;
-            continue
-        end
-    end
-    
-    % if there is overlap
-    if combLM(i,4) == 1
-        next_too = 1;
-        
-        % allow up to max_num movements to combine
-        for ii = 1:(max_num - 1)
-            if combLM(i + next_too,4) == 1
-                next_too = next_too + 1;
-            end                        
-        end
-        % if more than max_num, just leave
-        if next_too >= max_num
-            i = i+next_too; % skip all of the combined movements
-            continue
-        end
-        CLM = [CLM ; [combLM(i,1), combLM(i+next_too,2), 3]];
-        i = i+next_too;
-    % else, we're just a lonely unilateral mvmt. keep us
-    else
-        CLM = [CLM ; combLM(i,[1:2,5])];
-        i = i+1;
-    end
-end
 
-% move the leg indicator to the 13th column (out of the way of other values
-if ~isempty(CLM)
-    CLM(:,13) = CLM(:,3); CLM(:,3) = 0;
+while i < size(CLM,1)
+    % make sure to check if this is correct logic for the half second
+    % overlap period...
+    if isempty(intersect(CLM(i,1):CLM(i,2),(CLM(i+1,1)-fs/2):CLM(i+1,2)))
+        i = i+1;       
+    else
+        CLM(i,2) = max(CLM(i,2),CLM(i+1,2));
+        CLM(i,4) = CLM(i,4) + CLM(i+1,4);
+        CLM(i,13) = 3;
+        CLM(i+1,:) = [];
+    end
 end
 
 end
