0706a8108598164801f2f89c2f03ca7a6b3e0455
pskeba1 <pskeba1@jhu.edu>
2016-05-19 10:32:38 -0400

    greatly improved runtime with median filter

---
+++ cutLowMedian_rev1.m
@@ -0,0 +1,18 @@
+function nLM = cutLowMedian_rev1(LM,EMG,fs,t)
+
+nLM = LM * 0; j = 1;
+
+for i = 1:size(LM,1)
+    data = EMG(LM(i,1):LM(i,2),1);
+    
+    d = medfilt2(data,[fs/2,1]);
+    d(:,2) = t(LM(i,1):LM(i,2));
+    
+    if sum(d(:,1) > d(:,2)) > 0
+        % nLM = [nLM ; LM(i,:)];
+        nLM(j,:) = LM(i,:);
+        j = j+1;
+    end
+end
+
+nLM = nLM(1:j,:);
