6fc76aa4b4f57371703c3d0db6efa6a43def2113
pskeba1 <pskeba1@jhu.edu>
2016-05-31 09:49:22 -0400

    use uigetfile

--- EDF Conversion/techplm.m
+++ EDF Conversion/techplm.m
@@ -1,7 +1,8 @@
 %function PLM = techplm()
 % can be used for batch processing with a little work
 
-f = 'D:\Glutamate Study\AidRLS, G59734_V1N2 - 1_30_2014\G59734_V1N2 AidRLS-Events PLM.txt';
+[f,p] = uigetfile('D:\Glutamate Study\*PLM.txt', 'Open the PLM text file:' );
+f = [p f];
 
 T = readtable(f,'headerlines',15);
 formatIn = 'yyyy-mm-ddTHH:MM:SS'; % original didn't have milliseconds
@@ -20,4 +21,4 @@ tPLM(:,1) = etime(datevec(T.Var1(:),formatIn),datevec(starttime(:,1),formatIn));
 tPLM(:,1) = tPLM(:,1) * 500;
 tPLM(:,2) = tPLM(:,1) + T.Var3(:)*500;
 
-clear T starttime f formatIn
+clear T starttime f formatIn plms p
