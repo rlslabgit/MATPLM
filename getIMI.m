2bd08262abc796e5bbc76fd938c48b20bb9b9322
pskeba1 <pskeba1@jhu.edu>
2016-05-31 09:53:14 -0400

    backed up these old files on disk

--- getIMI.m
+++
@@ -1,9 +0,0 @@
-%% getIMI calculates intermovement interval and stores in the fourth column
-%  of the input array. IMI is onset-to-onset and measured in seconds
-function LM = getIMI(LM,fs) 
-
-LM(1,4) = 9999; % archaic... don't know if we need this
-LM(2:end,4) = (LM(2:end,1) - LM(1:end-1,1))/fs;
-
-
-end
