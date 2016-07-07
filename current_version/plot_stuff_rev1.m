57c539b4503f2f29f5bae8667117ba17860b53aa
pskeba1 <pskeba1@jhu.edu>
2016-07-07 13:03:17 -0400

    minor changes

--- current_version/plot_stuff_rev1.m
+++ current_version/plot_stuff_rev1.m
@@ -29,10 +29,10 @@ t = (1:size(lEMG))/fs/24/3600; t = t';
 t = t + datenum(plm_outputs.hypnostart);
 
 h(1) = subplot(2,1,1); reduce_plot(t,lEMG);
-legend('mLM', 'CLM', 'PLM', 'PLMS');
+legend('none', 'mLM', 'PLM', 'PLMS');
 datetickzoom('x','HH:MM:SS');
 h(2) = subplot(2,1,2); reduce_plot(t,rEMG);
-legend('mLM', 'CLM', 'PLM', 'PLMS');
+legend('none', 'mLM', 'PLM', 'PLMS');
 linkaxes(h,'x'); datetickzoom('x','HH:MM:SS');
 
 
