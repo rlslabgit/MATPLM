b26fee820e2de28aa1d1d72742ca431921dab952
pskeba1 <pskeba1@jhu.edu>
2017-01-19 16:58:39 -0500

    clean up

--- current_version/bullshit_TOTAL.m
+++
@@ -1,5 +0,0 @@
-addpath(['C:\Users\Administrator\Documents\GitHub\'...
-    'MATPLM (rlslabgit)\EDF Conversion']);
-
-EDF = convert_1sub_rev1('D:\Glutamate Study\');
-[plm_outputs, lEMG, rEMG] = matplm_new_main_rev1(EDF);
